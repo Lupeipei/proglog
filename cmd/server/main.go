da061207a73fc89c23f1e60d6cbb8795f5d9695e
lucia <luciacachn@gmail.com>
2021-05-18 15:56:52 +0800

    start up

---
+++ cmd/server/main.go
@@ -0,0 +1,10 @@
+package main
+import (
+  "log"
+  "github.com/lupeipei/proglog/internal/server"
+)
+
+func main() {
+  srv := server.NewHTTPServer(":8080")
+  log.Fatal(srv.ListenAndServe())
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Lupeipei/proglog
+
+go 1.14
